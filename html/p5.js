3e4b65ce0dc8d356d5ba8610bfa00ff8481b7f2d
Jenny0628 <39579942+Jenny0628@users.noreply.github.com>
2018-06-20 00:06:55 +0800

    Add files via upload

---
+++ html/p5.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="../css/appstyle.css">
+        <title>site of JFS</title>
+    </head>
+
+    <body>
+        <div id="image-main" src="C:\Users\Jenny\Desktop\j.jpg">
+            <article class="font">
+                <h2 class="header">p5.js</h2>
+                <a href="C:\Users\Jenny\Desktop\web design sample\homework result\Assgnt_1_A0563017">p5.js</br></a>
+                </br>
+                </p>
+            </article>
+
+            <nav class="font">
+                <h1 class="header">p5.js</h1>
+                </br>
+                <a href="../index.html">Home</br></a> 
+            </nav>
+
+        </div>
+    </body>
+</html>
